a284cdf2a68eda363e65e7a794d6b911f1b749f2
志明陈 <czmtxz@gmail.com>
2026-05-31 19:38:51 +0800

    Auto sync 2026-05-31 19:38:51

--- tools/_schema.py
+++ tools/_schema.py
@@ -2,8 +2,9 @@
 import sqlite3, os
 DB = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'project_manager.db')
 db = sqlite3.connect(DB)
-print(db.execute("SELECT sql FROM sqlite_master WHERE type='table' AND name='sales_orders'").fetchone()[0])
+t = os.environ.get('T', 'sales_order_items')
+print(db.execute("SELECT sql FROM sqlite_master WHERE type='table' AND name=?", (t,)).fetchone()[0])
 print('--- columns ---')
-for r in db.execute("PRAGMA table_info(sales_orders)").fetchall():
+for r in db.execute("PRAGMA table_info(%s)" % t).fetchall():
     print(r)
 db.close()
